6238d191ab9d191a5eb1e3100940d12af47190c1
m2nseop <pms246@naver.com>
2024-02-18 16:34:51 +0900

    feat: cascade, 연관관계 메서드 구현

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,7 @@ package com.jpabook.jpashop.domain;
 
 import static jakarta.persistence.FetchType.*;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -37,10 +38,10 @@ public class Order {
     // foreign key가 관계중 갑이 된다. // 따로 설정할 필요는 없고 을의 관계에만 설정을 해준다.
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // Order를 영속화(디비에 저장)하면 orderItems를 따로 영속화하도록 명령하지 않아도 알아서 영속화해주며 삭제 또한 그렇다 .
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -48,4 +49,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // enum 타입 // 주문상태 [ORDER, CANCEL]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
