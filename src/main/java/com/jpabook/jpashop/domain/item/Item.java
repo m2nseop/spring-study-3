b7e650e3385c8dd576ebfbac560a955553ec4367
m2nseop <pms246@naver.com>
2024-02-21 15:31:37 +0900

    feat: item엔티티 비즈니스 로직 추가

--- src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpabook.jpashop.domain.item;
 
 import com.jpabook.jpashop.domain.Category;
+import com.jpabook.jpashop.exception.NotEnoughStockException;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
@@ -31,4 +32,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // 도메인 주도 설계할 떄 객체지향적인 관점에서
+    // 엔티티 내에서 해결할 수 있는 것들에 대한 비즈니스 로직은 엔티티 내부에 둔다.
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+
+    }
 }
