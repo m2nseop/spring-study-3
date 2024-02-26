80cd8b3769905873a6aafe3e19478f0001ef4155
m2nseop <pms246@naver.com>
2024-02-26 16:29:08 +0900

    feat: 회원가입 화면 및 기능 구현

---
+++ src/main/java/com/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,45 @@
+package com.jpabook.jpashop.controller;
+
+import com.jpabook.jpashop.domain.Address;
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createFrom(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            // @Valid에서 에러가 걸리면 이에 대한 처리를 여기서 해줄 수 있다.
+            // 원래는 화이트라벨 오류 페이지가 떠야하지만, 서비스에서 그렇게 둘 수는 없기 때문
+            // 따라서 다시 회원가입창으로 돌려보내준다.
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
