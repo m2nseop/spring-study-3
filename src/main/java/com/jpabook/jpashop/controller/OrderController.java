4e7daec1fab7a00c350be924dd3ebfc9938adbd0
m2nseop <pms246@naver.com>
2024-03-04 17:31:20 +0900

    feat: 상품 주문 기능 구현:

---
+++ src/main/java/com/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,46 @@
+package com.jpabook.jpashop.controller;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.domain.item.Item;
+import com.jpabook.jpashop.service.ItemService;
+import com.jpabook.jpashop.service.MemberService;
+import com.jpabook.jpashop.service.OrderService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        // to do
+        // 여러개의 상품도 주문할 수 있도록 바꿔볼 것
+        return "redirect:/orders";
+    }
+}
