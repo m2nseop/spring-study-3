5b2322d5622e6adb312d8162df4caeec48e989fd
m2nseop <pms246@naver.com>
2024-03-05 22:18:17 +0900

    fix: 상품 수정 기능 버그 수정

--- src/main/java/com/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -74,7 +74,7 @@ public class ItemController {
         book.setIsbn(form.getIsbn());
 
         itemService.saveItem(book);
-        return "redirect:items";
+        return "redirect:/items";
     }
 }
 
